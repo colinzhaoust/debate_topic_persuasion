ebdda8c93dc3a324d3c68447b34a1322cf786bb1
colinzhaoust <1013224545@qq.com>
2020-11-24 07:38:21 +0800

    updated the experiment and data analysis

--- topic.py
+++ topic.py
@@ -67,7 +67,7 @@ class TopicModel(torch.nn.Module):
 
         self.issue_specific = torch.nn.Linear(10, 10)
 
-        self.text_specific1 = torch.nn.Linear(1024*2, 128)
+        self.text_specific1 = torch.nn.Linear(768*2, 128)
         self.text_specific2 = torch.nn.Linear(128, 128)
         self.text_specific3 = torch.nn.Linear(128, 128)
 
@@ -167,7 +167,7 @@ class RemoveTopic(TopicModel):
     def __init__(self, config):
         super().__init__(config)
 
-        self.text_specific1 = torch.nn.Linear(1024, 128)
+        self.text_specific1 = torch.nn.Linear(768, 128)
         self.classification = torch.nn.Linear(128+64+10, self.hidden_dim) # remaining
 
     def forward(self, cat_vec, ling_vec, tokenized_sents1, tokenized_sents2, issue_emb):
@@ -217,7 +217,7 @@ class SBERT(torch.nn.Module):
 
         self.issue_specific = torch.nn.Linear(10, 10)
 
-        self.text_specific1 = torch.nn.Linear(1024*2, 128)
+        self.text_specific1 = torch.nn.Linear(768*2, 128)
         self.text_specific2 = torch.nn.Linear(128, 128)
         self.text_specific3 = torch.nn.Linear(128, 128)
 
@@ -504,6 +504,14 @@ def train_test_each(args, tensor_split):
         current_model = OneHot(args.model)
 
     current_model.to(device)
+
+    # continue training
+    if args.continue_train:
+        try:
+            current_model.load_state_dict(torch.load('./best_models/' + args.model + "_" + args.baseline + '.pth'))
+        except:
+            a = "do nothing"
+
     optimizer = torch.optim.SGD(current_model.parameters(), lr=args.lr)
     loss_func = torch.nn.CrossEntropyLoss()
     logging.disable(logging.NOTSET)
@@ -518,14 +526,14 @@ def train_test_each(args, tensor_split):
         if f1 > best_dev_performance:
             print("New Best F1!!! " + str(f1))
             best_dev_performance = f1
-            torch.save(current_model.state_dict(), './best_models/' + args.model + "_" + args.baseline + '.pth')
+            torch.save(current_model.state_dict(), './best_models_continue/' + args.model + "_" + args.baseline + '.pth')
 
-    current_model.load_state_dict(torch.load('./best_models/' + args.model + "_" + args.baseline + '.pth'))
+    current_model.load_state_dict(torch.load('./best_models_continue/' + args.model + "_" + args.baseline + '.pth'))
 
     acc, p, r, f1, label_seq, pred_seq = test(current_model, test_data, args, loss_func, optimizer)
     all_devs.append([acc, p, r, f1, label_seq, pred_seq])
 
-    with open('./best_models/' + args.model + "_" + args.baseline + '.json', "w", encoding="UTF-8") as f:
+    with open('./best_models_continue/' + args.model + "_" + args.baseline + '.json', "w", encoding="UTF-8") as f:
         json.dump(all_devs, f)
 
 
@@ -541,7 +549,7 @@ parser.add_argument("--gpu", default='0', type=str, required=False,
                     help="choose which gpu to use")
 parser.add_argument("--seed", default=2020, type=int, required=False,
                     help="default random seed")
-parser.add_argument("--model", default='bert-large-uncased', type=str, required=False,
+parser.add_argument("--model", default='bert-base-uncased', type=str, required=False,
                     help="choose the model to test")
 parser.add_argument("--lr", default=0.0005, type=float, required=False,
                     help="initial learning rate")
@@ -555,6 +563,8 @@ parser.add_argument("--baseline", default="full", type=str, required=False,
                     help="the baseline to test") # full, sbert, no_debater, no_argument, no_topic, one_hot
 parser.add_argument("--max_sent_num", default=5, type=int, required=False,
                     help="max number of sentences to process")
+parser.add_argument("--continue_train", default=False, type=bool, required=False,
+                    help="train with the previous best model")
 
 
 args = parser.parse_args()
